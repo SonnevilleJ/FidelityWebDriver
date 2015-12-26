05fbb24357817414cf88e46c5d5d87b3fca1097f
John Sonneville <SonnevilleJ@gmail.com>
2015-12-26 14:33:32 -0600

    Refactored test.

--- FidelityWebDriver.Tests/Positions/PositionsPageTests.cs
+++ FidelityWebDriver.Tests/Positions/PositionsPageTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Moq;
 using NUnit.Framework;
@@ -16,52 +17,14 @@ namespace Sonneville.FidelityWebDriver.Tests.Positions
         private PositionsPage _positionsPage;
         private Mock<IWebDriver> _webDriverMock;
         private Mock<IPageFactory> _pageFactoryMock;
-        private List<IWebElement> _accountDivs;
-        private List<Dictionary<string, string>> _startingValues;
-
-        private readonly IDictionary<string, AccountType> _accountTypeStrings = new Dictionary<string, AccountType>
-        {
-            {"IA", AccountType.InvestmentAccount},
-            {"RA", AccountType.RetirementAccount},
-            {"HS", AccountType.HealthSavingsAccount},
-            {"OA", AccountType.Other},
-            {"CC", AccountType.CreditCard},
-        };
-
-        private readonly Dictionary<string, string> _valueAttributeNames = new Dictionary<string, string>
-        {
-            {"IA", "data-most-recent-value"},
-            {"RA", "data-most-recent-value"},
-            {"HS", "data-most-recent-value"},
-            {"OA", "data-most-recent-value"},
-            {"CC", "data-acct-balance"}
-        };
-
-        private IDictionary<string, Mock<IWebElement>> _groupDivMocks;
+        private List<Dictionary<string, string>> _expectedAccountDetails;
 
         [SetUp]
         public void Setup()
         {
-            _startingValues = new List<Dictionary<string, string>>
-            {
-                CreateAccountValues("abc1234", "IA", "my taxable investment account", "123.45678"),
-                CreateAccountValues("bl54321", "RA", "my BrokerageLink account", "98765.43211"),
-                CreateAccountValues("hsa9876", "HS", "my health savings account", "1357.02468"),
-                CreateAccountValues("401kplan", "OA", "my 401(k) account", "0"),
-                CreateAccountValues("credit1", "CC", "my first credit card", "1234.56"),
-                CreateAccountValues("credit2", "CC", "my second credit card", "12.34"),
-            };
-
-            _groupDivMocks = new Dictionary<string, Mock<IWebElement>>();
-
-            _accountDivs = _startingValues
-                .GroupBy(d => d["accountType"])
-                .Select(CreateAccountGroupDivMock)
-                .ToList();
-
             _webDriverMock = new Mock<IWebDriver>();
             _webDriverMock.Setup(driver => driver.FindElements(By.ClassName("account-selector--group-container")))
-                .Returns(_accountDivs.AsReadOnly());
+                .Returns(SetupAccountDivs());
 
             _pageFactoryMock = new Mock<IPageFactory>();
 
@@ -71,17 +34,46 @@ namespace Sonneville.FidelityWebDriver.Tests.Positions
         [Test]
         public void ShouldReturnOneAccountPerAccountOnPage()
         {
+            var accountTypeStrings = new Dictionary<string, AccountType>
+            {
+                {"IA", AccountType.InvestmentAccount},
+                {"RA", AccountType.RetirementAccount},
+                {"HS", AccountType.HealthSavingsAccount},
+                {"OA", AccountType.Other},
+                {"CC", AccountType.CreditCard},
+            };
+
             var accounts = _positionsPage.BuildAccounts().ToList();
 
-            Assert.AreEqual(_startingValues.Count(), accounts.Count());
+            Assert.AreEqual(_expectedAccountDetails.Count(), accounts.Count());
             foreach (var account in accounts)
             {
-                var matchingValues = _startingValues.Single(values => values["accountNumber"] == account.AccountNumber);
+                var matchingValues =
+                    _expectedAccountDetails.Single(values => values["accountNumber"] == account.AccountNumber);
 
                 Assert.AreEqual(matchingValues["accountName"], account.Name);
-                Assert.AreEqual(_accountTypeStrings[matchingValues["accountType"]], account.AccountType);
+                Assert.AreEqual(accountTypeStrings[matchingValues["accountType"]], account.AccountType);
                 Assert.AreEqual(double.Parse(matchingValues["accountValue"]), account.MostRecentValue);
             }
+        }
+
+        private ReadOnlyCollection<IWebElement> SetupAccountDivs()
+        {
+            _expectedAccountDetails = new List<Dictionary<string, string>>
+            {
+                CreateAccountValues("abc1234", "IA", "my taxable investment account", "123.45678"),
+                CreateAccountValues("bl54321", "RA", "my BrokerageLink account", "98765.43211"),
+                CreateAccountValues("hsa9876", "HS", "my health savings account", "1357.02468"),
+                CreateAccountValues("401kplan", "OA", "my 401(k) account", "0"),
+                CreateAccountValues("credit1", "CC", "my first credit card", "1234.56"),
+                CreateAccountValues("credit2", "CC", "my second credit card", "12.34"),
+            };
+
+            return _expectedAccountDetails
+                .GroupBy(d => d["accountType"])
+                .Select(CreateAccountGroupDivs)
+                .ToList()
+                .AsReadOnly();
         }
 
         private Dictionary<string, string> CreateAccountValues(string accountNumber, string accountType,
@@ -96,40 +88,44 @@ namespace Sonneville.FidelityWebDriver.Tests.Positions
             };
         }
 
-        private IWebElement CreateAccountGroupDivMock(
+        private IWebElement CreateAccountGroupDivs(
             IGrouping<string, IReadOnlyDictionary<string, string>> accountValuesByAccountType)
         {
-            var accountTypeString = accountValuesByAccountType.Key;
-
-            var accountGroupDiv = GetOrCreateAccountGroupDiv(accountTypeString);
-            accountGroupDiv.Setup(div => div.GetAttribute("data-group-id")).Returns(accountTypeString);
-            var accountDivs = CreateAccountDivs(accountValuesByAccountType, accountTypeString);
-            accountGroupDiv.Setup(div => div.FindElements(By.ClassName("js-account")))
-                .Returns(accountDivs.ToList().AsReadOnly());
-            return accountGroupDiv.Object;
+            var accountGroupDivMock = new Mock<IWebElement>();
+            accountGroupDivMock.Setup(div => div.GetAttribute("data-group-id")).Returns(accountValuesByAccountType.Key);
+            accountGroupDivMock.Setup(div => div.FindElements(By.ClassName("js-account")))
+                .Returns(CreateAccountDivs(accountValuesByAccountType));
+            return accountGroupDivMock.Object;
         }
 
-        private IEnumerable<IWebElement> CreateAccountDivs(
-            IEnumerable<IReadOnlyDictionary<string, string>> accountValues, string accountTypeString)
+        private ReadOnlyCollection<IWebElement> CreateAccountDivs(
+            IGrouping<string, IReadOnlyDictionary<string, string>> accountValuesByAccountType)
         {
-            return accountValues.Select(values =>
+            return accountValuesByAccountType
+                .Select(values => CreateWebElementForAccountValues(accountValuesByAccountType.Key, values))
+                .ToList()
+                .AsReadOnly();
+        }
+
+        private static IWebElement CreateWebElementForAccountValues(string accountType,
+            IReadOnlyDictionary<string, string> values)
+        {
+            var valueAttributeNames = new Dictionary<string, string>
             {
-                var accountDivMock = new Mock<IWebElement>();
-                accountDivMock.Setup(div => div.GetAttribute("data-acct-number"))
-                    .Returns(values["accountNumber"]);
-                accountDivMock.Setup(div => div.GetAttribute("data-acct-name"))
-                    .Returns(values["accountName"]);
-                accountDivMock.Setup(div => div.GetAttribute(_valueAttributeNames[accountTypeString]))
-                    .Returns(values["accountValue"]);
-                return accountDivMock.Object;
-            });
-        }
-
-        private Mock<IWebElement> GetOrCreateAccountGroupDiv(string accountTypeString)
-        {
-            return _groupDivMocks.ContainsKey(accountTypeString)
-                ? _groupDivMocks[accountTypeString]
-                : new Mock<IWebElement>();
+                {"IA", "data-most-recent-value"},
+                {"RA", "data-most-recent-value"},
+                {"HS", "data-most-recent-value"},
+                {"OA", "data-most-recent-value"},
+                {"CC", "data-acct-balance"}
+            };
+            var accountDivMock = new Mock<IWebElement>();
+            accountDivMock.Setup(div => div.GetAttribute("data-acct-number"))
+                .Returns(values["accountNumber"]);
+            accountDivMock.Setup(div => div.GetAttribute("data-acct-name"))
+                .Returns(values["accountName"]);
+            accountDivMock.Setup(div => div.GetAttribute(valueAttributeNames[accountType]))
+                .Returns(values["accountValue"]);
+            return accountDivMock.Object;
         }
     }
 }
