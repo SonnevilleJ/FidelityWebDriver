2b8b15f30d8ffdcb5e0b79c4f1266ecb2b6a5aa4
John Sonneville <SonnevilleJ@gmail.com>
2017-04-18 21:01:16 -0500

    Add NumberParser to log before numeric parsing.

---
+++ FidelityWebDriver.Tests/Utilities/NumberParserTest.cs
@@ -0,0 +1,111 @@
+﻿using System.Globalization;
+using log4net;
+using Moq;
+using NUnit.Framework;
+using Sonneville.FidelityWebDriver.Utilities;
+
+namespace Sonneville.FidelityWebDriver.Tests.Utilities
+{
+    [TestFixture]
+    public class NumberParserTest
+    {
+        private Mock<ILog> _logMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logMock = new Mock<ILog>();
+
+            NumberParser.SetLogger(_logMock.Object);
+        }
+
+        [Test]
+        [TestCase("0", 0)]
+        [TestCase("0.0", 0)]
+        [TestCase("1.0", 1)]
+        [TestCase("-1.0", -1)]
+        public void ShouldParseDoubles(string input, double expected)
+        {
+            var actual = NumberParser.ParseDouble(input);
+
+            Assert.AreEqual(expected, actual);
+            _logMock.Verify(log => log.Debug(It.Is<object>(o => o.ToString().Contains(input))));
+        }
+
+        [Test]
+        [TestCase("0")]
+        [TestCase("0.0")]
+        [TestCase("1.0")]
+        [TestCase("-1.0")]
+        public void ShouldParseDecimals(string input)
+        {
+            var actual = NumberParser.ParseDecimal(input);
+
+            Assert.IsAssignableFrom<decimal>(actual);
+            Assert.AreEqual(input, actual.ToString(CultureInfo.InvariantCulture));
+            _logMock.Verify(log => log.Debug(It.Is<object>(o => o.ToString().Contains(input))));
+        }
+
+        [Test]
+        [TestCase("0", 0)]
+        [TestCase("1", 1)]
+        [TestCase("-1", -1)]
+        [TestCase("2147483647", 2147483647)]
+        [TestCase("-2147483648", -2147483648)]
+        public void ShouldParseIntegers(string input, double expected)
+        {
+            var actual = NumberParser.ParseInt(input);
+
+            Assert.AreEqual(expected, actual);
+            _logMock.Verify(log => log.Debug(It.Is<object>(o => o.ToString().Contains(input))));
+        }
+
+        [Test]
+        [TestCase("0", 0)]
+        [TestCase("1", 1)]
+        [TestCase("-1", -1)]
+        [TestCase("2147483648", 2147483648)]
+        [TestCase("-2147483649", -2147483649)]
+        [TestCase("9223372036854775807", 9223372036854775807)]
+        [TestCase("-9223372036854775808", -9223372036854775808)]
+        public void ShouldParseLongs(string input, long expected)
+        {
+            var actual = NumberParser.ParseLong(input);
+
+            Assert.AreEqual(expected, actual);
+            _logMock.Verify(log => log.Debug(It.Is<object>(o => o.ToString().Contains(input))));
+        }
+
+        [Test]
+        public void ShouldParseDoubleWithoutProvidedLogger()
+        {
+            NumberParser.SetLogger();
+
+            Assert.AreEqual(5.5, NumberParser.ParseDouble("5.5"));
+        }
+
+        [Test]
+        public void ShouldParseDecimalWithoutProvidedLogger()
+        {
+            NumberParser.SetLogger();
+
+            Assert.AreEqual(5.5M, NumberParser.ParseDecimal("5.5"));
+        }
+
+        [Test]
+        public void ShouldParseIntWithoutProvidedLogger()
+        {
+            NumberParser.SetLogger();
+
+            Assert.AreEqual(5, NumberParser.ParseInt("5"));
+        }
+
+        [Test]
+        public void ShouldParseLongWithoutProvidedLogger()
+        {
+            NumberParser.SetLogger();
+
+            Assert.AreEqual(5L, NumberParser.ParseLong("5"));
+        }
+    }
+}
---
+++ FidelityWebDriver/Utilities/NumberParser.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using log4net;
+
+namespace Sonneville.FidelityWebDriver.Utilities
+{
+    public static class NumberParser
+    {
+        private static ILog _log;
+
+        public static void SetLogger(ILog log = null)
+        {
+            _log = log ?? LogManager.GetLogger(typeof(NumberParser));
+        }
+
+        public static double ParseDouble(string input, NumberStyles numberStyles = NumberStyles.Any)
+        {
+            Log<double>(input, numberStyles);
+            return double.Parse(input, numberStyles);
+        }
+
+        public static decimal ParseDecimal(string input, NumberStyles numberStyles = NumberStyles.Any)
+        {
+            Log<decimal>(input, numberStyles);
+            return decimal.Parse(input, numberStyles);
+        }
+
+        public static int ParseInt(string input, NumberStyles numberStyles = NumberStyles.Any)
+        {
+            Log<int>(input, numberStyles);
+            return int.Parse(input, numberStyles);
+        }
+
+        public static long ParseLong(string input, NumberStyles numberStyles = NumberStyles.Any)
+        {
+            Log<long>(input, numberStyles);
+            return long.Parse(input, numberStyles);
+        }
+
+        private static void Log<T>(string input, NumberStyles numberStyles)
+        {
+            _log?.Debug($"Parsing '{input}' into a {typeof(T)} using NumberStyles.{numberStyles}...");
+        }
+    }
+}
